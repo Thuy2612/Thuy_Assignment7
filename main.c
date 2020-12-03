45e2d47fe39b9b3781f69db8804f79cf04ad1c75
Thuy2612 <jacksonthu2811@gmail.com>
2020-12-03 08:33:03 +0700

    Modify1

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/*******************************************************************************
+* Declaration Function
+******************************************************************************/
+bool allocate10Bytes(uint8_t *outPtr);
+
+/*******************************************************************************
+* main coding
+******************************************************************************/
+int main(int argc, char *argv[]) {
+    uint8_t *outPtr=NULL;
+    int i;
+    bool check = allocate10Bytes(&outPtr);
+
+    if(check)
+    {
+        printf("Memory successfully allocated!");
+        printf("\nAddreess: %x",outPtr);
+    }
+    else
+    {
+        printf("\nMemory not allocated!");
+        exit(0);
+    }
+    return 0;
+}
+
+/*FUNCTION**********************************************************************
+ *
+ * Function Name : allocate10Bytes
+ * Description   : Memory allocated using malloc
+ *
+ *END**************************************************************************/
+bool allocate10Bytes(uint8_t *outPtr)
+{
+	uint8_t **m=(uint8_t**)outPtr;
+	
+    *m=(uint8_t*)malloc(10*sizeof(uint16_t));
+    if(outPtr==NULL)
+        return false;
+    else
+    {
+        return true;
+    }
+}
